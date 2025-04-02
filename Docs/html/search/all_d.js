8e2d087425dd99f525df5de3807a3a7abab8a548
Jace <jacemcclenny@gmail.com>
2025-04-01 23:00:42 -0500

    fixed a minor Doxygen issue

--- Docs/html/search/all_d.js
+++ Docs/html/search/all_d.js
@@ -4,6 +4,5 @@ var searchData=
   ['lcs_1',['LCS',['../class_l_c_s.html',1,'LCS'],['../class_l_c_s.html#abd38e95e77a6fe4d0fedbf57cc648463',1,'LCS::LCS(string a, string b)']]],
   ['lcslength_2',['lcsLength',['../class_l_c_s.html#a7ea748b0ca0561880d92cdef60e1b09e',1,'LCS']]],
   ['length_3',['length',['../class_circular_dynamic_array.html#a8e78b28ba277933867296ab3a045815f',1,'CircularDynamicArray']]],
-  ['license_4',['License',['..//Users/jace/Documents/GitHub/CPP-Structs-Algorithms/README.md#autotoc_md15',1,'']]],
-  ['linearsearch_5',['linearSearch',['../class_circular_dynamic_array.html#ad49cca26dc5bd6cdeb6be9a6e2903fe6',1,'CircularDynamicArray']]]
+  ['linearsearch_4',['linearSearch',['../class_circular_dynamic_array.html#ad49cca26dc5bd6cdeb6be9a6e2903fe6',1,'CircularDynamicArray']]]
 ];
